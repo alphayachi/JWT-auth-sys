e9103267d262ae91a29855c9ceeaedc35541e342
ayaachi <ayaachi712@gmail.com>
2021-04-02 15:54:04 +0530

    title change and form validation check

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = ({ isAuthenticated, logout }) =>
     return (
         <Fragment>
             <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-                <Link className='navbar-brand' to='/'>MailZX</Link>
+                <Link className='navbar-brand' to='/'>Authentication System</Link>
                 <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
                     <span className='navbar-toggler-icon'></span>
                 </button>
